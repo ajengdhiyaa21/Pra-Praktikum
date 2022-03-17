260f9e96cd2ffb47082707b03e7b0c9dcac40404
ajengdhiyaa21 <ajengdpq@gmail.com>
2022-03-18 00:05:42 +0700

    membuat header file

---
+++ Pra Praktikum/kalkulator.h
@@ -0,0 +1,7 @@
+#ifndef _KALKULATOR_H
+#define _KALKULATOR_H
+float Penambahan(int a, int b);
+float pengurangan(int a, int b);
+float perkalian(int a,int b);
+float pembagian(int a, int b);
+#endif
