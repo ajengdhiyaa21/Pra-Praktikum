39f9b82a8ab0f2ba0845802c7c8c7fe19e411d42
amayliaacn <amayliaaaaa25@gmail.com>
2022-03-18 01:06:27 +0700

    menambahkan file baru

---
+++ Pra Praktikum/kalkulator.c
@@ -0,0 +1,13 @@
+#include "kalkulator.h"
+float Penambahan(int a, int b){
+	return (a+b);
+}
+float Pengurangan(int a, int b){
+	return (a-b);
+}
+float Perkalian(int a, int b){
+	return (a*b);
+}
+float Pembagian(int a, int b){
+	return (a/b);
+}
