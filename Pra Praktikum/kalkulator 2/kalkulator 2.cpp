ae466a06c9632f74e157393a760cc9208948ec0d
ajengdhiyaa21 <ajengdpq@gmail.com>
2022-03-18 23:54:12 +0700

    menambah file kalkulator ke 2

---
+++ Pra Praktikum/kalkulator 2/kalkulator 2.cpp
@@ -0,0 +1,22 @@
+#include <conio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	
+	int bil1,bil2, pil;
+	float hasil;
+	string operasi;
+	
+	cout<<"PILIH OPERATOR ARITMATIKA"<<endl;
+	cout<<"1. Penjumlahan"<<endl;
+	cout<<"2. Pengurangan"<<endl;
+	cout<<"3. Perkalian"<<endl;
+	cout<<"4. Pembagian"<<endl;
+	cout<<"5. Modulus"<<endl;
+	cout<<endl;
+	
+	getch();
+}
